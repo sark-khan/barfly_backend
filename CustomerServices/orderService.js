ab0c1d7f79d843d97a3525cdf75b4156b3669510
Sarik Khan <sarikalan@webmobsoft.com>
2025-03-21 19:49:12 +0530

    changes for the socket

--- CustomerServices/orderService.js
+++ CustomerServices/orderService.js
@@ -106,7 +106,7 @@ const createOrder = async (req, session) => {
     await Discount.updateOne({ code: couponCode }, { $inc: { usedCount: 1 } });
   }
   console.log({r: req.userId});
-  io.to(entityId).emit("newOrder", response);
+  io.to(entityId).emit("newOrder", createdOrder);
   return createdOrder;
 };
 
