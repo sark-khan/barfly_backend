3db7b497a15c04403fd5bebb3a9f84b399455661
“Atul” <atul.rajput@webmobinfo.ch>
2025-03-31 11:28:28 +0530

    changes

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -3,17 +3,23 @@ const { getMessaging } = require("firebase-admin/messaging");
 
 // Import your service account keys for both projects
 const serviceAccount = require("./Utils/firebaseKey.json");
-const serviceAccountPlus = require("./Utils/firebaseCountrplusKey.json");
+// const serviceAccountPlus = require("./Utils/firebaseCountrplusKey.json");
 
 // Initialize Firebase Admin SDK for the Default App (Project 1)
-const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-}, "default");  // Give it a name 'default'
+const app = admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccount),
+  },
+  "default"
+); // Give it a name 'default'
 
 // Initialize Firebase Admin SDK for the Second App (Project 2)
-const countrPlusApp = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountPlus),
-}, "countrPlusApp");  // Provide a unique name for the second app instance
+const countrPlusApp = admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccountPlus),
+  },
+  "countrPlusApp"
+); // Provide a unique name for the second app instance
 
 // Get messaging instances for both projects
 const messaging = getMessaging(admin.app("default"));
