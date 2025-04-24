b7fe43fe4ec5c9e9240d455f572baa600c60d0b0
“Atul” <atul.rajput@webmobinfo.ch>
2025-04-24 22:26:57 +0530

    changes

--- Controller/stripeController.js
+++ Controller/stripeController.js
@@ -51,11 +51,9 @@ router.get("/get-payment-status", async (req, res) => {
 router.post("/account-link", async (req, res) => {
   try {
     const response = await createStripeOnboardingLink(req);
-    return res
-      .status(STATUS_CODES.OK)
-      .json({ message: "Account link created", response });
+    return res.status(STATUS_CODES.OK).json(response);
   } catch (error) {
-    console.error("Error while creating the account");
+    console.error("Error while creating the account:", error);
     res.status(STATUS_CODES.SERVER_ERROR).json({ error: error.message });
   }
 });
