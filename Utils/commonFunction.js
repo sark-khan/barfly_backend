95b44e70ad262ce2f6a828111505025f0d803009
“Atul” <atul.rajput@webmobinfo.ch>
2025-04-28 19:12:33 +0530

    chnages in the registration floe

--- Utils/commonFunction.js
+++ Utils/commonFunction.js
@@ -37,6 +37,7 @@ const getJwtToken = (user, isUser = false) => {
     email: user.email,
     contactNumber: user.contactNumber,
     isAdmin: user.isAdmin,
+    countrTag: user.countrTag,
   };
   if (!isUser) {
     payload = {
@@ -209,7 +210,7 @@ const decrypt = (encryptedText) => {
 };
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 module.exports = {
