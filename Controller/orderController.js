7b83903151dbed001cc0f5d3c3c2d916409143e3
“Atul” <atul.rajput@webmobinfo.ch>
2025-05-08 13:40:26 +0530

    changes for the socket

--- Controller/orderController.js
+++ Controller/orderController.js
@@ -41,7 +41,8 @@ router.post("/create-order", async (req, res) => {
     await session.withTransaction(async () => {
       response = await createOrder(req, session);
     });
-    io.to(req.entityId.toString()).emit("newOrder");
+    console.log({ response });
+    io.to(response.entityId.toString()).emit("newOrder");
 
     // const userDetails = await User.findById(req.userId, { socketId: 1 });
     // if (userDetails && userDetails.socketId) {
