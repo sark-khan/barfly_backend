bb10af920acb0559320c5dade4151f981906bf11
“Atul” <atul.rajput@webmobinfo.ch>
2025-05-14 13:40:57 +0530

    changes in admin

--- CustomerServices/stripeServices.js
+++ CustomerServices/stripeServices.js
@@ -256,7 +256,7 @@ const createStripeOnboardingLink = async (req) => {
 
   const isMobile = platform === "app";
   const redirectUrl = isMobile
-    ? `${process.env.HOST_URL}/countrplus://bank`
+    ? `${process.env.HOST_URL}/stripe/redirect-bank?platform=app`
     : `${process.env.HOST_URL}/app/profile/bank-account`;
 
   const accountLink = await stripe.accountLinks.create({
