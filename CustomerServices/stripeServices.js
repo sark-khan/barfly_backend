346ac5ea4283c37a91aa4c19cfdc94f9776dd122
“Atul” <atul.rajput@webmobinfo.ch>
2025-05-14 18:08:59 +0530

    changes in stripe url

--- CustomerServices/stripeServices.js
+++ CustomerServices/stripeServices.js
@@ -257,7 +257,7 @@ const createStripeOnboardingLink = async (req) => {
   const isMobile = platform === "app";
   const redirectUrl = isMobile
     ? // ? `${process.env.HOST_URL}/stripe/redirect-bank?platform=app`
-      `${process.env.HOST_URL}/.well-known/apple-app-site-association`
+      `${process.env.HOST_URL}//onboarding`
     : `${process.env.HOST_URL}/app/profile/bank-account`;
 
   const accountLink = await stripe.accountLinks.create({
@@ -442,7 +442,8 @@ const continueStripeOnboarding = async (req) => {
 
     const isMobile = platform === "app";
     const redirectUrl = isMobile
-      ? `${process.env.HOST_URL}/stripe/redirect-bank?platform=app`
+      ? // ? `${process.env.HOST_URL}/stripe/redirect-bank?platform=app`
+        `${process.env.HOST_URL}//onboarding`
       : `${process.env.HOST_URL}/app/profile/bank-account`;
 
     const accountLink = await stripe.accountLinks.create({
