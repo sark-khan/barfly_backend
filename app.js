19c598d00c55ce99694ca541a65b1a47a2ef594c
“Atul” <atul.rajput@webmobinfo.ch>
2025-05-15 17:22:21 +0530

    changes in the stripe

--- app.js
+++ app.js
@@ -1,70 +1,85 @@
 const express = require("express");
-// require("dotenv").config();
 require("./db");
 require("./redis");
 require("./cron/emitEvent");
 require("./server");
 require("./Utils/bullQueue");
 require("./Utils/emitProcessor");
+// const setupCron = require("./cron/cron");
 
 const path = require("path");
+const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const multer = require("multer");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
-
 const { setIo } = require("./Utils/socket");
+const stripe = require("stripe");
 const StripeModel = require("./Models/Stripe");
-const Stripe = require("stripe");
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+  transports: ["websocket", "polling"],
+});
+
+const { orderSocket } = require("./server");
+orderSocket(io);
+setIo(io);
+
+// setupCron(io);
+module.exports = { io };
+
+const orderController = require("./Controller/orderController");
+const StripeController = require("./Controller/stripeController");
+const Counter = require("./Models/Counter");
+const adminController = require("./Admin/controller");
+const multer = require("multer");
+const {
+  uploadBufferToS3,
+  generatePresignedUrl,
+} = require("./Controller/aws-service");
+const ItemDetails = require("./Models/ItemDetails");
+const MenuItem = require("./Models/MenuItem");
+const Admin = require("./Models/Admin");
 const {
   STATUS_CODES,
   ORDER_STATUS,
   STRIPE_PAYMENT_STATUS,
 } = require("./Utils/globalConstants");
-
-const {
-  uploadBufferToS3,
-  generatePresignedUrl,
-} = require("./Controller/aws-service");
 const { ownerTrades } = require("./PdfServices/ownerTrades");
 const verifyToken = require("./Utils/verifyToken");
 const { sendFirebaseNotification } = require("./Utils/commonFunction");
-
-const orderController = require("./Controller/orderController");
-const StripeController = require("./Controller/stripeController");
-const adminController = require("./Admin/controller");
-
-const Counter = require("./Models/Counter");
-const ItemDetails = require("./Models/ItemDetails");
-const MenuItem = require("./Models/MenuItem");
-const Admin = require("./Models/Admin");
 const User = require("./Models/User");
-
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
+// const admin = require("./firebaseConfig");
 
 const unProtectedApis = {
   "/api/customer/auth/login": true,
   "/api/customer/auth/register": true,
   "/api/customer/auth/countR-tag": true,
+  // "/api/customer/entities/get-entities": true,
   "/api/owner/auth/register": true,
   "/api/owner/auth/login": true,
   "/api/customer/auth/check-and-generate-countR-tag": true,
+  "/api/owner/auth/register": true,
+  "/api/owner/auth/login": true,
+
   "/api/customer/entities/get-menu-category-items": true,
   "/api/customer/entities/recommended-items": true,
+  // "/api/get-trade-pdf": true,
   "/api/owner/restaurant/email-exist": true,
   "/api/admins/login-admin": true,
   "/api/admins/reset-password": true,
+  // "/api/admins/add-admin": true,
+
   "/api/stripe/get-stripe-accounts": true,
-  "/webhooks": true,
 };
 
 app.use("/api/health-check", (req, res) => {
@@ -76,16 +91,17 @@ app.use("/api/health-check", (req, res) => {
 
 app.use("/.well-known", express.static(path.join(__dirname, ".well-known")));
 
+// Optional: root route
 app.get("/", (req, res) => {
   res.send("Apple Pay Domain Verification Running");
 });
 
 app.use((req, res, next) => {
   if (unProtectedApis[req.path]) return next();
+
   return verifyToken(req, res, next);
 });
 
-// API Routes
 app.use(
   "/api/owner/auth",
   require("./Controller/Owner/Authentication/controller")
@@ -97,78 +113,46 @@ app.use(
   require("./Controller/Customer/Authentication/controller")
 );
 app.use("/api/customer/entities", require("./Controller/Customer/controller"));
+
 app.use("/api/orders", orderController);
 app.use("/api/stripe", StripeController);
 app.use("/api/admins", adminController);
 
-// Stripe Webhook
-app.post(
-  "/webhooks",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    console.log("Received webhook call");
-    const sig = req.headers["stripe-signature"];
-    let event;
+const preloadPlatformFees = async () => {
+  const admin = await Admin.findOne({ isAdmin: true }).lean();
+  global.PLATFORM_FEES = admin?.platformFees || 0;
+};
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
-      console.error("Webhook signature verification failed.", err.message);
-      return res
-        .status(STATUS_CODES.BAD_REQUEST)
-        .send(`Webhook Error: ${err.message}`);
-    }
+preloadPlatformFees();
 
-    const intent = event.data.object;
-    const paymentIntentId = intent.id;
-
-    switch (event.type) {
-      case "payment_intent.succeeded":
-        console.log(`Payment intent succeeded: ${paymentIntentId}`);
-        break;
-      case "payment_intent.payment_failed":
-        console.log(`Payment intent failed: ${paymentIntentId}`);
-        break;
-      case "payment_intent.canceled":
-        console.log(`Payment intent canceled: ${paymentIntentId}`);
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    res.sendStatus(STATUS_CODES.OK);
-  }
-);
-
-// Custom Routes
 app.post("/api/update-menu-items", async (req, res) => {
   try {
-    const result = await Counter.updateMany(
+    const getMenuitems = await Counter.updateMany(
       {},
       { $set: { isTableService: false, isSelfPickUp: true, totalTables: 0 } }
     );
-    return res.status(200).json(result);
+    return res.status(200).json(getMenuitems);
   } catch (error) {
-    console.log("Error in update-menu-items", error);
+    console.log("error occured in update-menu");
     return res.status(500).json({ error });
   }
 });
 
 app.post("/api/upload-file", upload.single("file"), async (req, res) => {
   try {
-    if (!req.file) return res.status(400).send("No file uploaded.");
+    if (!req.file) {
+      return res.status(400).send("No file uploaded.");
+    }
+    const fileBuffer = req.file.buffer;
+    const fileName = "coca-cola.png";
 
-    const data = await uploadBufferToS3(req.file.buffer, "coca-cola.png");
+    const data = await uploadBufferToS3(fileBuffer, fileName);
     return res.status(200).json({
       message: "Uploaded successfully",
       location: data.Location,
     });
   } catch (error) {
-    console.log("Error in upload-file", error);
+    console.log("error occured in update-menu", error);
     return res.status(500).json({ error });
   }
 });
@@ -179,24 +163,22 @@ app.get("/api/download-file", async (req, res) => {
     const fileStream = generatePresignedUrl(fileName);
     return res.status(200).json({ fileStream });
   } catch (error) {
-    console.log("Error in download-file", error);
+    console.log("error occured in update-menu", error);
     return res.status(500).json({ error });
   }
 });
 
 app.post("/update-entity-items", async (req, res) => {
   try {
-    const items = await ItemDetails.find({ entityId: req.entityId }).lean();
-    const itemIds = items.map((item) => item.itemId);
-
+    console.log({ ee: req.entityId });
+    const itemsId = await ItemDetails.find({ entityId: req.entityId }).lean();
+    console.log({ itemsId });
+    const itemIds = itemsId.map((item) => item.itemId);
     await MenuItem.updateMany(
       { _id: { $in: itemIds } },
       { $set: { entityId: req.entityId } }
     );
-
-    return res
-      .status(STATUS_CODES.OK)
-      .json({ message: "Updated all the docs" });
+    return res.status(STATUS_CODES.OK).json({ message: "Updated all the doc" });
   } catch (error) {
     return res
       .status(STATUS_CODES.SERVER_ERROR)
@@ -209,7 +191,7 @@ app.get("/api/get-trade-and-download-pdf", async (req, res) => {
     const signedUrl = await ownerTrades(req);
     return res.status(STATUS_CODES.OK).json({ downloadUrl: signedUrl });
   } catch (error) {
-    console.error("Error while creating trade PDF", error);
+    console.error("Error occurred while creating trade PDF", error);
     return res
       .status(STATUS_CODES.SERVER_ERROR)
       .json({ message: "PDF creation failed", error });
@@ -232,41 +214,74 @@ app.post("/api/register-token", async (req, res) => {
   const { fcmToken } = req.body;
 
   if (!fcmToken) {
-    return res.status(STATUS_CODES.BAD_REQUEST).json({
+    throw {
+      status: STATUS_CODES.BAD_REQUEST,
       message: "FCM token is required",
-    });
+    };
   }
 
   await User.updateOne({ _id: req.userId }, { $addToSet: { fcmToken } });
 
-  return res.json({ message: "FCM token registered successfully" });
+  return { message: "FCM token registered successfully" };
 });
 
-// Preload global fees
-const preloadPlatformFees = async () => {
-  const admin = await Admin.findOne({ isAdmin: true }).lean();
-  global.PLATFORM_FEES = admin?.platformFees || 0;
-};
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    let event;
 
-preloadPlatformFees();
+    try {
+      const sig = req.headers["stripe-signature"];
+      event = stripe.webhooks.constructEvent(
+        req.body,
+        sig,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.error("Webhook signature verification failed.", err.message);
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send(`Webhook Error: ${err.message}`);
+    }
 
-// Socket.IO Setup
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-  transports: ["websocket", "polling"],
-});
+    const intent = event.data.object;
+    const paymentIntentId = intent.id;
 
-const { orderSocket } = require("./server");
-orderSocket(io);
-setIo(io);
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        await StripeModel.updateOne(
+          { stripePaymentIntentId: paymentIntentId },
+          { $set: { paymentStatus: STRIPE_PAYMENT_STATUS.SUCCESSFUL } }
+        );
+        break;
 
-// Start server
+      case "payment_intent.payment_failed":
+        await StripeModel.updateOne(
+          { stripePaymentIntentId: paymentIntentId },
+          { $set: { paymentStatus: STRIPE_PAYMENT_STATUS.FAILED } }
+        );
+        break;
+
+      case "payment_intent.canceled":
+        await StripeModel.updateOne(
+          { stripePaymentIntentId: paymentIntentId },
+          { $set: { paymentStatus: STRIPE_PAYMENT_STATUS.CANCELLED } }
+        );
+        break;
+
+      // Add other statuses if needed
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    res.sendStatus(STATUS_CODES.OK);
+  }
+);
+
 const port = process.env.PORT;
+
 server.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
-
-module.exports = { io };
