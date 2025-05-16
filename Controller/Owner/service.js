b2eee34ef22e10fabd8129c78c31a40dd937c22e
Sarik Khan <sarikalan@webmobsoft.com>
2025-05-17 01:29:08 +0530

    changes for the foreb ase counter

--- Controller/Owner/service.js
+++ Controller/Owner/service.js
@@ -104,17 +104,7 @@ module.exports.createCounter = async (req) => {
 
   io.to(newCounter.entityId.toString()).emit("newCounter", newCounter);
   
-  sendFirebaseNotification({
-      topic: `entity_${newCounter.entityId}`,
-      showNotification: false,
-      title: "New Counter Added",
-      body: "You have a new counter added. Tap to view.",
-      data: {
-            action:"counter_update",
-            screen: "landing_home",
-            click_action: "FLUTTER_NOTIFICATION_CLICK",
-          },
-    });
+  
   const lastTable = await Tables.findOne(
     { entityId: req.entityId },
     { tableSetionNo: 1 }
@@ -132,68 +122,80 @@ module.exports.createCounter = async (req) => {
     status: STATUS.ACTIVE,
   });
 
-  const owner = await User.findOne(
-    {
-      _id: newCounter.ownerId,
-      fcmToken: { $exists: true, $not: { $size: 0 } },
-    },
-    { fcmToken: 1 }
-  );
+  // const owner = await User.findOne(
+  //   {
+  //     _id: newCounter.ownerId,
+  //     fcmToken: { $exists: true, $not: { $size: 0 } },
+  //   },
+  //   { fcmToken: 1 }
+  // );
 
-  if (!owner?.fcmToken?.length) return newCounter.toObject();
 
-  const notificationPayload = (token) => ({
-    notification: {
-      title: "New Counter Created",
-      body: `Counter "${counterName}" is now available.`,
-    },
+  sendFirebaseNotification({
+    topic: `entity_${newCounter.entityId}`,
+    showNotification: false,
+    title: "New Counter Added",
+    body: "You have a new counter added. Tap to view.",
     data: {
-      screen: "counter",
-      entityId: req.entityId.toString(),
-      click_action: "FLUTTER_NOTIFICATION_CLICK",
-    },
-    token,
-    android: {
-      priority: "high",
-      notification: {
-        click_action: "FLUTTER_NOTIFICATION_CLICK",
-      },
-    },
-    apns: {
-      payload: {
-        aps: {
-          content_available: true,
-          alert: {
-            title: "New Counter Created",
-            body: `Counter "${counterName}" is now available.`,
-          },
-          category: "FLUTTER_NOTIFICATION_CLICK",
-          mutableContent: 1,
+          action:"counter_update",
+          screen: "landing_home",
+          click_action: "FLUTTER_NOTIFICATION_CLICK",
         },
-      },
-    },
   });
+  // if (!owner?.fcmToken?.length) return newCounter.toObject();
 
-  for (const token of owner.fcmToken) {
-    try {
-      await messagingPlus.send(notificationPayload(token));
-      console.log(`Notification sent to token: ${token}`);
-    } catch (err) {
-      console.error("FCM push failed for token:", token, err.message);
+  // const notificationPayload = (token) => ({
+  //   notification: {
+  //     title: "New Counter Created",
+  //     body: `Counter "${counterName}" is now available.`,
+  //   },
+  //   data: {
+  //     screen: "counter",
+  //     entityId: req.entityId.toString(),
+  //     click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //   },
+  //   token,
+  //   android: {
+  //     priority: "high",
+  //     notification: {
+  //       click_action: "FLUTTER_NOTIFICATION_CLICK",
+  //     },
+  //   },
+  //   apns: {
+  //     payload: {
+  //       aps: {
+  //         content_available: true,
+  //         alert: {
+  //           title: "New Counter Created",
+  //           body: `Counter "${counterName}" is now available.`,
+  //         },
+  //         category: "FLUTTER_NOTIFICATION_CLICK",
+  //         mutableContent: 1,
+  //       },
+  //     },
+  //   },
+  // });
 
-      // Optional: Remove invalid tokens
-      if (
-        err.code === "messaging/invalid-argument" ||
-        err.code === "messaging/registration-token-not-registered" ||
-        err.code === "messaging/invalid-recipient"
-      ) {
-        await User.updateOne(
-          { _id: newCounter.ownerId },
-          { $pull: { fcmToken: token } }
-        );
-      }
-    }
-  }
+  // for (const token of owner.fcmToken) {
+  //   try {
+  //     await messagingPlus.send(notificationPayload(token));
+  //     console.log(`Notification sent to token: ${token}`);
+  //   } catch (err) {
+  //     console.error("FCM push failed for token:", token, err.message);
+
+  //     // Optional: Remove invalid tokens
+  //     if (
+  //       err.code === "messaging/invalid-argument" ||
+  //       err.code === "messaging/registration-token-not-registered" ||
+  //       err.code === "messaging/invalid-recipient"
+  //     ) {
+  //       await User.updateOne(
+  //         { _id: newCounter.ownerId },
+  //         { $pull: { fcmToken: token } }
+  //       );
+  //     }
+  //   }
+  // }
 
   return newCounter.toObject();
 };
