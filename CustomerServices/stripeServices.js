921cf5d94d7b6bea3218a6e9c085b08aef0514a9
bharti <bharti@webmobsoft.com>
2025-12-05 18:28:38 +0530

    remove company name

--- CustomerServices/stripeServices.js
+++ CustomerServices/stripeServices.js
@@ -132,7 +132,6 @@ const createPaymentIntent = async (req) => {
           },
         ],
         mode: "payment",
-        company_name: "Countr Customer",
         payment_intent_data: {
           application_fee_amount: feeInCents,
           transfer_data: {
