cea10216581f72d25a376a89acf08c5df622e25a
Sarik Khan <sarikalan@webmobsoft.com>
2026-03-05 20:38:29 +0530

    changes for the token

--- Utils/commonFunction.js
+++ Utils/commonFunction.js
@@ -5,6 +5,7 @@ const SECRET_KEY = "BARFLY@WEBMOB456";
 const Event = require("../Models/Event");
 const { STATUS, ROLES, STATUS_CODES } = require("./globalConstants");
 const Discount = require("../Models/Discount");
+const User = require("../Models/User");
 const crypto = require("crypto");
 const { messaging, messagingPlus } = require("../firebaseAdmin");
 const { getLanguageFromRequest, t } = require("./translator");
@@ -413,10 +414,14 @@ const verifyTokenWithoutResponse = async (req) => {
       const redisKey = `${KEY_TYPE_PREFIXES.USER_TOKEN}:${decoded.userId}`;
       const sessionExists = await redisClient.get(redisKey);
       if (!sessionExists) {
-        throwError({
-          status: STATUS_CODES.NOT_AUTHORIZED,
-          message: t("USER_ACCOUNT_BLOCKED", lang),
-        });
+        const user = await User.findById(decoded.userId, { status: 1 }).lean();
+        if (user?.status === STATUS.BLOCKED) {
+          throwError({
+            status: STATUS_CODES.NOT_AUTHORIZED,
+            message: t("USER_ACCOUNT_BLOCKED", lang),
+          });
+        }
+        await redisClient.set(redisKey, "1");
       }
     }
   } catch (err) {
