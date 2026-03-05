11ff076169c94f8c975460efd6bcce5e28132b18
Sarik Khan <sarikalan@webmobsoft.com>
2026-03-06 02:16:46 +0530

    changes for the data

--- CustomerServices/orderService.js
+++ CustomerServices/orderService.js
@@ -320,7 +320,7 @@ const updateStatusOfOrder = async (req) => {
   // Send Firebase notification to owner app for order status update
   sendFirebaseNotification({
     topic: `owner_entity_${order.entityId}`,
-    showNotification: false,
+    showNotification: true,
     title: "Order Status Updated",
     body: `Order #${order.tokenNumber} is now ${status}.`,
     data: {
